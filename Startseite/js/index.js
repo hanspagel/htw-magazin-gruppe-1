0f725f63ddb14ede1808fb0005155129d13b2f03
janospower <pauer.janos@gmail.com>
2017-11-25 15:22:09 +0100

    Fine tuning scrolling behavior

--- Startseite/js/index.js
+++ Startseite/js/index.js
@@ -4,33 +4,17 @@ window.onload = function() {
   var stack = document.getElementsByClassName('card-stack')[0];
   // add 5 because inline-block sucks and i'm too lazy to deal with flexbox
   var cardWidth = document.getElementsByClassName("card")[0].offsetWidth + 5;
-  console.log(cardWidth);
-  var stackWidth =
+  var stackWidth = cardWidth * 15;
+
   // update position of square 1 ands when scroll event fires.
   window.addEventListener('scroll', function() {
     var scrollLeft = window.pageXOffset || window.scrollLeft;
-    var scrollPercent = scrollLeft/3175 || 0;
+    // var scrollPercent = scrollLeft/(stackWidth-window.innerWidth) || 0;
+    var scrollPercent = scrollLeft/3205 || 0;
 
     stack.style.transform = "rotate(-13deg) translateY("+
-     scrollPercent*window.innerWidth
+     scrollPercent
     + "px)";
+    console.log(scrollPercent);
   });
 };
-
-
-
-// window.onload = function() {
-//   var container = document.getElementById('container');
-//   var windowHeight = window.innerHeight;
-//   var windowWidth = window.innerWidth;
-//   var scrollArea = 1000 - windowHeight;
-//   var square1 = document.getElementsByClassName('card-stack')[0];
-//
-//   // update position of square 1 ands when scroll event fires.
-//   window.addEventListener('scroll', function() {
-//     var scrollLeft = window.pageYOffset || window.scrollLeft;
-//     var scrollPercent = scrollLeft/scrollArea || 0;
-//
-//     square1.style.transform = "rotate(-13deg) translateY("+scrollPercent*window.innerWidth + "px)";
-//   });
-// };
