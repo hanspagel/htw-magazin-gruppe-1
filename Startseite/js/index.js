6fac6105e2602bd2fcaf142d79fe479c5d1fb4b8
janospower <pauer.janos@gmail.com>
2017-11-25 16:08:09 +0100

    scrolling container instead of body fixes overflow-xissues

--- Startseite/js/index.js
+++ Startseite/js/index.js
@@ -1,5 +1,6 @@
 
 window.onload = function() {
+  var windowc = document.getElementsByTagName("main")[0];
   var windowWidth = window.innerWidth;
   var stack = document.getElementsByClassName('card-stack')[0];
   // add 5 because inline-block sucks and i'm too lazy to deal with flexbox
@@ -7,19 +8,19 @@ window.onload = function() {
   var stackWidth = cardWidth * 15;
 
   // update position of square 1 ands when scroll event fires.
-  window.addEventListener('scroll', function() {
-    var scrollLeft = window.pageXOffset || window.scrollLeft;
+  windowc.addEventListener('scroll', function() {
+    var scrollLeft = windowc.pageXOffset || windowc.scrollLeft;
     // var scrollPercent = scrollLeft/(stackWidth-window.innerWidth) || 0;
     var scrollPercent = scrollLeft/3205 || 0;
 
     stack.style.transform = "rotate(-13deg) translateY("+
      scrollPercent * 693
     + "px)";
-    // console.log(scrollPercent);
+    console.log(scrollPercent);
 
     // stop vertical scrool. super-jittery
-    if (document.body.scrollTop!=0) {
-      document.body.scrollTop = 0;
-    }
+    // if (document.body.scrollTop!=0) {
+    //   document.body.scrollTop = 0;
+    // }
   });
 };
