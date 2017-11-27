2dee8ed5a2959486957c05cfd68cd5e57903b185
Lea Gl <leagl@live.de>
2017-11-27 10:00:19 +0100

    Java

--- Svenleaja/app.js
+++ Svenleaja/app.js
@@ -1,2 +1,20 @@
 
-alert("Dieser Inhalt ist für über 65jährige nicht geeignet.")
+$(document).ready(function(){
+// NOTE: ready= wenn der browser bereit first
+
+  $(".button").click(function() {
+
+// NOTE: toggleClass=lichtschalter für button (gif an/ausschalten)
+    // NOTE: addClass=gif wird bei klicken abgespielt in endlosschleife
+    $(".article-interactive").toggleClass("playdrücken")
+
+
+
+
+  })
+
+
+
+
+
+  })
