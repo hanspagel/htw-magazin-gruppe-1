9a34b6f583e103b6895837672dae0289eb740f0c
Philine Schell <gwen.finley.gf@gmail.com>
2017-11-28 14:22:46 +0100

    voller Boxen

--- rollercoaster_tycoon/app.js
+++ rollercoaster_tycoon/app.js
@@ -5,16 +5,31 @@ $(window).scroll(function() {
   var scroll = getCurrentScroll();
 
 
-    if ( scroll >= 1000 ) {
-      $('.box-voll').addClass('box-voll-voll-burger');
+    if ( scroll >= 4400 ) {
+      $('.box-voll').addClass('box-voll-audio');
       $('.box-outline').addClass('box-voll-voll-burger');
+    }
+    else if ( scroll >= 3800 ) {
+      $('.box-voll').addClass('box-voll-person');
+    }
+    else if ( scroll >= 3200 ) {
+      $('.box-voll').addClass('box-voll-kopf');
+    }
+    else if ( scroll >= 2500 ) {
+      $('.box-voll').addClass('box-voll-ballon');
+    }
+    else if ( scroll >= 1700 ) {
+      $('.box-voll').addClass('box-voll-zange');
+    }
+    else if ( scroll >= 1000 ) {
+      $('.box-voll').addClass('box-voll-start');
     }
     else if ( scroll >= 500 ) {
       $('.box-voll').addClass('box-voll-burger');
     }
-    else {
+    /*else ( scroll >=  500 ) {
       $('.box-voll').removeClass('box-voll-burger');
-    }
+    }*/
 
 
 });
