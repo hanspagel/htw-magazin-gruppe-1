e209e75bc0661f697983dcf678dd9b7b8789d349
TanyaGryn <tanyagryn@ymail.com>
2017-12-04 13:56:28 +0100

    paralaxe

--- mariashenja/app.js
+++ mariashenja/app.js
@@ -4,4 +4,20 @@ $(document).ready(function() {
     $(".article-interactive").toggleClass("action")
   })
 
+  $(window).mousemove(function(event) {
+    var offsetX = 800 / $(document).width() * event.pageX;
+    var offsetY = 800 / $(document).height() * event.pageY;
+
+    $("#_8").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 700 / $(document).width() * event.pageX;
+    var offsetY = 700 / $(document).height() * event.pageY;
+
+    $("#_7").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+  })
+
 } )
