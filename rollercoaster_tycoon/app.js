2c095caf75ccdfa6de541e84ac4eae19afd3f4cb
Philine Schell <gwen.finley.gf@gmail.com>
2017-12-04 15:43:13 +0100

    sortieren plus Logo Farben

--- rollercoaster_tycoon/app.js
+++ rollercoaster_tycoon/app.js
@@ -5,29 +5,29 @@ $(window).scroll(function() {
   var scroll = getCurrentScroll();
 
     // console.log(scroll);
-    if ( (10000 >= scroll) && (scroll >= 4400) ) {
+    if ( (10000 >= scroll) && (scroll >= 6000) ) {
       changeClass('audio');
     }
-    else if ( (4400 >= scroll) && (scroll >= 3800) ) {
+    else if ( (6000 >= scroll) && (scroll >= 5000) ) {
       changeClass('person');
     }
-    else if ( (3800 >= scroll) && (scroll >= 3200) ) {
+    else if ( (5000 >= scroll) && (scroll >= 4000) ) {
       changeClass('kopf');
     }
-    else if ( (3200 >= scroll) && (scroll >= 2500) ) {
+    else if ( (4000 >= scroll) && (scroll >= 3500) ) {
       changeClass('ballon');
     }
-    else if ( (2500 >= scroll) && (scroll >= 1700) ) {
+    else if ( (3500 >= scroll) && (scroll >= 2800) ) {
       changeClass('zange');
     }
-    else if ( (1700 >= scroll) && (scroll >= 1000) ) {
+    else if ( (2800 >= scroll) && (scroll >= 1900) ) {
       changeClass('start');
     }
-    else if ( (1000 >= scroll) && (scroll >= 500) ) {
+    else if ( (1900 >= scroll) && (scroll >= 700) ) {
       changeClass('burger');
 
     }
-    else if ( scroll <=  500 ) {
+    else if ( scroll <=  700 ) {
       changeClass('zuckerwatte');
     }
 
@@ -37,7 +37,9 @@ function getCurrentScroll() {
   return window.pageYOffset || document.documentElement.scrollTop;
   }
 });
+
 function changeClass(classargs) {
   $('.box-voll').removeClass().addClass('box-voll box-voll-' + classargs);
   $('.box-outline').removeClass().addClass('box-outline box-outline-' + classargs);
+  $('.logo-schrift').removeClass().addClass('logo-schrift logo-schrift-' + classargs);
 }
