40e6bf59e56a712c73ff87a9c3c5e1ffdf7ef279
Miriam Zils <zilsmiriam@gmail.com>
2017-12-05 15:00:25 +0100

    Moblie Compatibility Changes

--- Design Details/js/index.js
+++ Design Details/js/index.js
@@ -1,23 +1,13 @@
 $(window).load(function() {
-	$('#fullpage').fullpage({
-		navigation: true,
-	});
-
-// audio-slider custom
-// $(".custom-handle").mousedown(function() {
-// 	$(".slider-handle").on("mousemove", function(){
-// 		$(".custom-handle").css("margin-left" === "event.pageX" + "px");
-// 	});
-// });
+		if (window.screen.width > 780){
+			$('#fullpage').fullpage({
+				navigation: true,
+			});
+	};
 
 	var bqheight = document.getElementsByTagName("blockquote")[0].clientHeight;
 	if (bqheight < 256 ){
 		$("blockquote").css("height",256+"px");
 	}
-	// audio-slider with jquery-ui
-	var slider = $(".unlock-slider").slider({
-			 classes: {
-					 "ui-slider-handle": "custom-handle"}
-	 });
-	$(".unlock-slider").slider
+
 	 });
