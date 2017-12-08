57e931c194181465d0fd9014ece9e86239c1c586
TanyaGryn <tanyagryn@ymail.com>
2017-12-08 20:52:26 +0100

    paralaxe

--- mariashenja/app.js
+++ mariashenja/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $(".article-interactive").toggleClass("action")
   })
 
-  $(window).mousemove(function(event) {
+    $(window).mousemove(function(event) {
     var offsetX = 800 / $(document).width() * event.pageX;
     var offsetY = 800 / $(document).height() * event.pageY;
 
@@ -18,6 +18,54 @@ $(document).ready(function() {
     $("#_7").css({
       transform: "translate(" + offsetX + "px, " + offsetY + "px)"
     })
+
+    $("#_6").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 600 / $(document).width() * event.pageX;
+    var offsetY = 600 / $(document).height() * event.pageY;
+
+
+    $("#_5").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 500 / $(document).width() * event.pageX;
+    var offsetY = 500 / $(document).height() * event.pageY;
+
+
+    $("#_4").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 400 / $(document).width() * event.pageX;
+    var offsetY = 400 / $(document).height() * event.pageY;
+
+    $("#_3").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 300 / $(document).width() * event.pageX;
+    var offsetY = 300 / $(document).height() * event.pageY;
+
+
+    $("#_2").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 200 / $(document).width() * event.pageX;
+    var offsetY = 200 / $(document).height() * event.pageY;
+
+    $("#_1").css({
+      transform: "translate(" + offsetX + "px, " + offsetY + "px)"
+    })
+
+    var offsetX = 100 / $(document).width() * event.pageX;
+    var offsetY = 100 / $(document).height() * event.pageY;
+
+
+
   })
 
 } )
