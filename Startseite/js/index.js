a2e8d0b1fe35640ba7c015cb5cba3c23d36fc3fa
janospower <pauer.janos@gmail.com>
2017-12-23 12:20:43 +0100

    added iframe and page transitions

--- Startseite/js/index.js
+++ Startseite/js/index.js
@@ -229,3 +229,16 @@ function scrollToX(scrollTargetX, speed, easing) {
 }
 
 // scroll it!
+
+// $(document).ready(function() {
+//
+//     $("a.transition").click(function(event){
+//         event.preventDefault();
+//         linkLocation = this.href;
+//         $("body").fadeOut(1000, redirectPage);
+//     });
+//
+//     function redirectPage() {
+//         window.location = linkLocation;
+//     }
+// });
