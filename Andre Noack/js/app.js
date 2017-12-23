a462b41a282e95cbeaec69b4e7ad1ad179d5bea3
nckbrlyn <herr.noack@gmx.de>
2017-12-24 00:32:43 +0100

    add new giphy, add hover-box + implemented into the code (first test) (credits to Piotr Elmanowski), also some corrections

--- Andre Noack/js/app.js
+++ Andre Noack/js/app.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-
-  $(".button").click(function() {
-
-    $(".giphy").toggleClass("action")
-
-  })
-
+	$(".nhl_map img").mousemove(function(e) {
+		$('.map_text').css('left', e.clientX + 15);
+		$('.map_text').css('top', e.clientY + 5);
+		$('.map_text').css('display', 'block');
+	});
+	$(".nhl_map").mouseout(function(e) {
+		$('.map_text').css('display', 'none');
+	});
 })
 
 $(document).ready(function() {
-
   $(".button").click(function() {
-
-    $(".giphy-fr").toggleClass("action")
-
+    $(".giphy").toggleClass("action")
   })
+})
 
+$(document).ready(function() {
+  $(".button").click(function() {
+    $(".giphy-fr").toggleClass("action")
+  })
 })
