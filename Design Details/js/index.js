390097203002990b4e03114d86edd5740975122d
janospower <pauer.janos@gmail.com>
2017-12-23 10:09:26 +0100

    connect audio player

--- Design Details/js/index.js
+++ Design Details/js/index.js
@@ -9,43 +9,59 @@ $(window).load(function() {
 			}
 	};
 
-var $audioPlayer = $(".audio-player");
-var $playButton = $('#play');
-var $progressBar = $("#progressbar");
-var $player = $('#player');
-var $duration = 0;
-var $volume = 0.75;
+var audio_player = $(".audio-player");
+var play_button = $('#play');
+var progress_bar = $("#progressbar");
+var player = document.getElementById('player');
+var duration = 0;
+var volume = 0.75;
 
+var time = $("#time");
 
 player.onloadedmetadata = function() {
-    var duration = $player.duration;
-    $progressBar.progressbar("option", { 'max' : duration });
+    duration = player.duration;
+    progress_bar.progressbar("option", { 'max' : duration });
 };
 
-$player.load();
-$player.volume = 1;
+player.load();
+player.volume = 0.75;
 player.addEventListener("timeupdate", function() {
-    $progressBar.progressbar('value', $player.currentTime);
-}, true);
+    progress_bar.progressbar('value', player.currentTime);
+    time.text(getTime(player.currentTime));
+}, false);
 
-$progressBar.progressbar({
+function getTime(t) {
+    var m=~~(t/60), s=~~(t % 60);
+    return (m<10?"0"+m:m)+':'+(s<10?"0"+s:s);
+}
+
+function getProgressBarClickInfo(progress_bar, e) {
+    var offset = progress_bar.position();
+    var x = e.pageX - offset.left; // or e.offsetX (less support, though)
+    var y = e.pageY - offset.top;  // or e.offsetY
+    var max = progress_bar.progressbar("option", "max");
+    var value = x * max / progress_bar.width();
+
+    return { x: x, y: y, max: max, value: value };
+}
+
+progress_bar.progressbar({
     value : player.currentTime,
 });
 
-$progressBar.click(function(e) {
+progress_bar.click(function(e) {
     var info = getProgressBarClickInfo($(this), e);
     player.currentTime = player.duration / info.max * info.value;
 });
 
-$playButton.click(function() {
+play_button.click(function() {
     player[player.paused ? 'play' : 'pause']();
     $(this).toggleClass("fa-play", !player.paused);
     $(this).toggleClass("fa-pause", player.paused);
 });
 
-	$('#play').click(function() {
-    $('.icon').toggleClass('pause');
-  });
+
+
 
 
 
