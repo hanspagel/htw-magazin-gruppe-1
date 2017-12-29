0b6480c17575895c022eb86d773366e55699e333
ElenaKunau <elena.kunau@outlook.de>
2017-12-29 14:48:27 +0100

    hide canvas

--- Künstliche Intelligenz/script.js
+++ Künstliche Intelligenz/script.js
@@ -1,5 +1,21 @@
 $(document).ready (function() {
   AOS.init({
    duration: 1200
-  });
+});
+
+var windowWidth = $(window).width();
+
+function checkWidth() {
+  if (windowWidth < 1440) {
+      $("#myCanvas").show()
+  }
+  if (windowWidth > 1440){
+      $("#myCanvas").hide()
+  }
+}
+checkWidth() ;
+
+$(window).resize(function() {
+ checkWidth() ;
+});
 })
