93a4846459c454af1e433dbff1b1f4357aeab18c
rebeccamer <rebecca.merdes@gmail.com>
2018-01-01 21:40:41 +0100

    trying and failing all day long

--- utopia/app.js
+++ utopia/app.js
@@ -1,10 +1,43 @@
 
-$(document).ready(function() {
 
-$(".button").click(function() {
 
-  $(".article-interactive").toggleClass("spacedonut")
 
-})
 
-})
+
+// $(document).ready(function() {
+//
+// $(".button").click(function() {
+//
+//   $(".article-interactive").toggleClass("spacedonut")
+//
+// })
+//
+// })
+
+//
+// $(document).ready(function() {
+//   $(window).scroll(funtion() {
+//     var scroll = getCurrentScroll();
+//
+//     if ( (3000 >= scroll) && (scroll >= 2500) ) {
+//       $('#profilepic').attr('src', 'images/Lenin.png');
+//     }
+//     else if ( (2500 >= scroll) && (scroll >= 2000) ) {
+//       $('#profilepic').attr('src', 'images/ErnstBloch.png');
+//     }
+//     else if ( (700 >= scroll) && (scroll >= 400) ) {
+//       $('#profilepic').attr('src', 'images/Obama.png');
+//     }
+//     else if ( (400 >= scroll) && (scroll >= 200) ) {
+//       $('#profilepic').attr('src', 'images/ThomasMorus_sw.png');
+//     }
+//     else if ( scroll <= 200 ) {
+//       $('#profilepic').attr('src', 'images/thomasM.png');
+//     }
+//   });
+//
+// function getCurrentScroll() {
+//   return window.pageYOffset ||
+//   document.documentElement.scrollTop;
+//   }
+// });
