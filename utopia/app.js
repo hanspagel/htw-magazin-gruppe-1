9721ac24f7897d5bebbefd8b7a8b5a2c1666c4a5
rebeccamer <rebecca.merdes@gmail.com>
2018-01-02 03:47:17 +0100

    fertig für heute
    newutopia so gut wie fertig: portraits tauchen passend zum text im rahmen auf, rahmen und porträts enden wenn tür kommt, tür ist link zu unterseite, link zu rollercoaster tyconn ist nur kleiner leuchtturm, teil des headers

--- utopia/app.js
+++ utopia/app.js
@@ -20,20 +20,35 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scroll = getCurrentScroll();
 
-    if ( (2000 >= scroll) && (scroll >= 1000) ) {
+    if ( (2000 >= scroll) && (scroll >= 860) ) {
+      $('#profilepic').attr('src', 'images/nothing.png');
+      document.getElementById("name").innerHTML = "";
+      $('#noframe').attr('src', 'images/nothing.png');
+    }
+    else if ( (860 >= scroll) && (scroll >= 720) ) {
       $('#profilepic').attr('src', 'images/lenin_b.png');
+      document.getElementById("name").innerHTML = "Wladimir Iljitsch Lenin";
+      $('#noframe').attr('src', 'images/rahmen.png');
     }
-    else if ( (1000 >= scroll) && (scroll >= 800) ) {
+    else if ( (720 >= scroll) && (scroll >= 570) ) {
       $('#profilepic').attr('src', 'images/hiero_g.png');
+      document.getElementById("name").innerHTML = "Hieronymus Bosch";
+      $('#noframe').attr('src', 'images/rahmen.png');
     }
-    else if ( (800 >= scroll) && (scroll >= 550) ) {
+    else if ( (570 >= scroll) && (scroll >= 450) ) {
       $('#profilepic').attr('src', 'images/jlennon_y.png');
+      document.getElementById("name").innerHTML = "John Lennon";
+      $('#noframe').attr('src', 'images/rahmen.png');
     }
-    else if ( (550 >= scroll) && (scroll >= 250) ) {
+    else if ( (450 >= scroll) && (scroll >= 200) ) {
       $('#profilepic').attr('src', 'images/henry8_o.png');
+      document.getElementById("name").innerHTML = "Henry VIII.";
+      $('#noframe').attr('src', 'images/rahmen.png');
     }
-    else if ( scroll <= 250 ) {
+    else if ( scroll <= 200 ) {
       $('#profilepic').attr('src', 'images/tmore_l.png');
+      document.getElementById("name").innerHTML = "Thomas More";
+      $('#noframe').attr('src', 'images/rahmen.png');
     }
   });
 
